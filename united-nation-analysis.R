8f6c1b309558ad3fc038a244be43349de9adc274
Rod Morgado <rodrigo@zooft.company>
2021-12-06 15:34:29 -0800

    plotting the trend of percent yes by year for United Kingdom

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -56,3 +56,19 @@ by_year_country <- votes_processed %>%
   group_by(year, country) %>%
   summarize(total = n(),
             percent_yes = mean(vote == 1))
+
+# Start with by_year_country dataset
+by_year_country <- votes_processed %>%
+  group_by(year, country) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
+
+# Print by_year_country
+by_year_country
+
+# Create a filtered version: UK_by_year
+UK_by_year <- by_year_country %>% filter(country == "United Kingdom")
+
+# Line plot of percent_yes over time for UK only
+ggplot(UK_by_year, aes(x = year, y = percent_yes)) +
+  geom_line()
