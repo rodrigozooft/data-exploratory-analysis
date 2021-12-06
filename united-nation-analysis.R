fb425eeb72e3802fc352adbd52e4530f2ed71d95
Rod Morgado <rodrigo@zooft.company>
2021-12-06 15:24:43 -0800

    plotting the votes of % yes across time

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -32,4 +32,16 @@ by_country %>% arrange(desc(percent_yes))
 by_country %>%
   arrange(percent_yes) %>%
   filter(total > 100)
-  
+
+# Define by_year
+by_year <- votes_processed %>%
+  group_by(year) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
+
+# Load the ggplot2 package
+library(ggplot2)
+
+# Create line plot
+ggplot(by_year, aes(x = year, y = percent_yes)) +
+  geom_line()
