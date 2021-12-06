8bbea165555991698bbbd31f2de1fe23a7fb1d71
Rod Morgado <rodrigo@zooft.company>
2021-12-06 15:58:19 -0800

    adding three more countries to compare them

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -108,3 +108,16 @@ filtered_6_countries <- by_year_country %>%
 ggplot(filtered_6_countries, aes(year, percent_yes)) +
   geom_line() +
   facet_wrap(~ country, scales = "free_y")
+
+# Add three more countries to this list
+countries <- c("United States", "United Kingdom",
+               "France", "Japan", "Brazil", "India", "Chile", "Canada", "Ukraine")
+
+# Filtered by_year_country: filtered_countries
+filtered_countries <- by_year_country %>%
+  filter(country %in% countries)
+
+# Line plot of % yes over time faceted by country
+ggplot(filtered_countries, aes(year, percent_yes)) +
+  geom_line() +
+  facet_wrap(~ country, scales = "free_y")
