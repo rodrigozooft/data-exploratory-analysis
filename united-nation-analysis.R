b1a687aa280d149edee37d11d29c06df741767a6
Rod Morgado <rodrigo@zooft.company>
2021-12-06 15:19:45 -0800

    working on sorting the data by percentage

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -1,2 +1,29 @@
 votes_processed %>% 
   summarize(total = n(), percent_yes = mean(vote == 1))
+
+# Change this code to summarize by year
+votes_processed %>%
+  group_by(year) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
+
+# Summarize by country: by_country
+by_country <- votes_processed %>%
+  group_by(country) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
+
+# You have the votes summarized by country
+by_country <- votes_processed %>%
+  group_by(country) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
+
+# Print the by_country dataset
+by_country
+
+# Sort in ascending order of percent_yes
+by_country %>% arrange(percent_yes)
+
+# Now sort in descending order
+by_country %>% arrange(desc(percent_yes))
