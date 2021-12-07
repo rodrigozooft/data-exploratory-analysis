9c5bd59c4557a84ce47d61a34f8904339aac0797
Rod Morgado <rodrigo@zooft.company>
2021-12-07 12:03:11 -0800

    unnesting the tidied column to show all the data available in the same tibble

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -176,3 +176,14 @@ by_year_country %>%
   nest(-country) %>%
   mutate(model = map(data, ~ lm(percent_yes ~ year, data = .))) %>%
   mutate(tidied = map(model, tidy))
+
+# Add one more step that unnests the tidied column
+country_coefficients <- by_year_country %>%
+  nest(-country) %>%
+  mutate(model = map(data, ~ lm(percent_yes ~ year, data = .)),
+         tidied = map(model, tidy)) %>%
+         unnest(tidied)
+
+
+# Print the resulting country_coefficients variable
+country_coefficients
