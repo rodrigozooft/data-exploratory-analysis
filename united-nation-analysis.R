d6ef86087487b2c10e75c7601a0ad40dd469adf4
Rod Morgado <rodrigo@zooft.company>
2021-12-07 12:01:20 -0800

    adding a new column with a tidied version of the model

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -168,3 +168,11 @@ by_year_country %>%
   nest(-country) %>%
   mutate(model = map(data, ~lm(percent_yes ~ year, data = .)))
   
+# Load the broom package
+library(broom)
+
+# Add another mutate that applies tidy() to each model
+by_year_country %>%
+  nest(-country) %>%
+  mutate(model = map(data, ~ lm(percent_yes ~ year, data = .))) %>%
+  mutate(tidied = map(model, tidy))
