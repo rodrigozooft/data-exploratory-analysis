21153d60b8782c63ff2e70b73abeefcb78ced9ad
Rod Morgado <rodrigo@zooft.company>
2021-12-07 13:43:10 -0800

    group by country, year and topic and then, summarize to get the total and percent_yes

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -265,3 +265,11 @@ votes_tidied <- votes_gathered %>%
                         co = "Colonialism",
                         ec = "Economic development"))
 
+# Print votes_tidied
+votes_tidied
+
+# Summarize the percentage "yes" per country-year-topic
+by_country_year_topic <- votes_tidied %>% group_by(country, year, topic) %>% summarize(total = n(), percent_yes = mean(vote == 1)) %>% ungroup()
+
+# Print by_country_year_topic
+by_country_year_topic
