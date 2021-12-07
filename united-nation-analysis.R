ead212341ea6a1e4121a5f0bc3b9f51b5e9c1337
Rod Morgado <rodrigo@zooft.company>
2021-12-07 11:59:49 -0800

    add a column called model to add a linear model for each country using map from purrr package

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -157,3 +157,14 @@ nested <- by_year_country %>%
 
 # Unnest the data column to return it to its original form
 nested %>% unnest(data)
+
+# Load tidyr and purrr
+library(tidyr)
+library(purrr)
+
+
+# Perform a linear regression on each item in the data column
+by_year_country %>%
+  nest(-country) %>%
+  mutate(model = map(data, ~lm(percent_yes ~ year, data = .)))
+  
