dbde782d32e0884594a9ff656cfa7e750a500e84
Rod Morgado <rodrigo@zooft.company>
2021-12-07 13:29:39 -0800

    filtering by United States and Colonialism and summarizing the data by percent_yes

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -229,3 +229,17 @@ descriptions
 
 # Join them together based on the "rcid" and "session" columns
 votes_joined <- votes_processed %>% inner_join(descriptions, by = c("rcid", "session"))
+
+# Filter for votes related to colonialism
+votes_joined %>% filter(co == 1)
+
+# Load the ggplot2 package
+library(ggplot2)
+
+# Filter, then summarize by year: US_co_by_year
+US_co_by_year <- votes_joined %>% filter(country == "United States", co == 1) %>% group_by(year) %>% summarize(percent_yes = mean(vote == 1))
+
+
+# Graph the % of "yes" votes over time
+ggplot(US_co_by_year, aes(x = year, y = percent_yes)) +
+  geom_line()
