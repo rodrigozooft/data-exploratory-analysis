d92a885140aa54aea7547bdd060f5c436bee55a1
Rod Morgado <rodrigo@zooft.company>
2021-12-07 13:06:39 -0800

    inner join votes processed table with description table

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -217,3 +217,15 @@ filtered_countries %>% arrange(desc(estimate))
 
 # Sort for the countries decreasing most quickly
 filtered_countries %>% arrange(estimate)
+
+# Load dplyr package
+library(dplyr)
+
+# Print the votes_processed dataset
+votes_processed
+
+# Print the descriptions dataset
+descriptions
+
+# Join them together based on the "rcid" and "session" columns
+votes_joined <- votes_processed %>% inner_join(descriptions, by = c("rcid", "session"))
