cb5df74e021ccca6ff6f794f89bbb9b9ee6f5a16
Rod Morgado <rodrigo@zooft.company>
2021-12-07 13:46:54 -0800

    plotting percent_yes for United States faceted by topic

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -273,3 +273,15 @@ by_country_year_topic <- votes_tidied %>% group_by(country, year, topic) %>% sum
 
 # Print by_country_year_topic
 by_country_year_topic
+
+# Load the ggplot2 package
+library(ggplot2)
+
+# Filter by_country_year_topic for just the US
+US_by_country_year_topic <- by_country_year_topic %>% filter(country == "United States")
+
+# Plot % yes over time for the US, faceting by topic
+ggplot(US_by_country_year_topic, aes(x = year, y = percent_yes)) + 
+geom_line() + facet_wrap(~topic)
+
+
