dc7f55bab9fe74706ff2a03f458fd596922f2f44
Rod Morgado <rodrigo@zooft.company>
2021-12-07 14:36:19 -0800

    filtering by the term == year and p.adjusting the p.value to filter oonly the significance

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -301,3 +301,8 @@ country_topic_coefficients <- by_country_year_topic %>%
 
 # Print country_topic_coefficients
 country_topic_coefficients
+
+# Create country_topic_filtered
+country_topic_filtered <- country_topic_coefficients %>% filter(term == "year") %>%
+    mutate(p.adjusted = p.adjust(p.value)) %>%
+    filter(p.adjusted < 0.05)
