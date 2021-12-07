f14dbfb6ce7caafd698ad32b8c20df948b816484
Rod Morgado <rodrigo@zooft.company>
2021-12-07 11:43:54 -0800

    using broom, tidy() function and bind_rows to merge data from two different models

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -127,3 +127,20 @@ library(broom)
 
 # Call the tidy() function on the US_fit object
 tidy(US_fit)
+
+# Linear regression of percent_yes by year for US
+US_by_year <- by_year_country %>%
+  filter(country == "United States")
+US_fit <- lm(percent_yes ~ year, US_by_year)
+
+# Fit model for the United Kingdom
+UK_by_year <- by_year_country %>%
+  filter(country == "United Kingdom")
+UK_fit <- lm(percent_yes ~ year, UK_by_year)
+
+# Create US_tidied and UK_tidied
+US_tidied <- tidy(US_fit)
+UK_tidied <- tidy(UK_fit)
+
+# Combine the two tidied models
+bind_rows(US_tidied, UK_tidied)
