62b0c3f4214fd06b91007cfe3c754d7b56d4fa14
Rod Morgado <rodrigo@zooft.company>
2021-12-07 12:09:50 -0800

    adjusting the p-values and adding the adjusted value to a new column

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -194,3 +194,13 @@ country_coefficients
 # Filter for only the slope terms
 country_coefficients %>%
     filter(term == "year")
+
+# Filter for only the slope terms
+slope_terms <- country_coefficients %>%
+  filter(term == "year")
+
+# Add p.adjusted column, then filter
+
+slope_terms %>%
+  mutate(p.adjusted = p.adjust(p.value)) %>%
+  filter(p.adjusted < 0.05)
