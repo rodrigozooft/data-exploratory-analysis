7dd6a4f78370eedfe5cd83eca05cedde743557ef
Rod Morgado <rodrigo@zooft.company>
2021-12-07 11:53:23 -0800

    nest data and unnest to show how it works

--- united-nation-analysis.R
+++ united-nation-analysis.R
@@ -150,3 +150,10 @@ library(tidyr)
 # Nest all columns besides country
 by_year_country %>%
     nest(-country)
+
+# All countries are nested besides country
+nested <- by_year_country %>%
+  nest(-country)
+
+# Unnest the data column to return it to its original form
+nested %>% unnest(data)
